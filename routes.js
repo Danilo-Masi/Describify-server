dfa8ac59e783c7f7462dfbf19ea50284928d2a4b
dmasiii <danilomasi999@gmail.com>
2024-11-02 21:15:32 +0100

    Describify server

--- routes.js
+++ routes.js
@@ -3,19 +3,19 @@ import waitlistRoutes from './routes/waitlistRoutes.js';
 // Route per l'invio delle email tramite Resend
 import emailSendRoutes from './routes/emailSendRoutes.js';
 // Route per il signin
-import signinRoutes from './routes/signinRoutes.js';
+//import signinRoutes from './routes/signinRoutes.js';
 // Route per il signup
-import signupRoutes from './routes/signupRoutes.js';
+//import signupRoutes from './routes/signupRoutes.js';
 // Route per il signout
-import signoutRoutes from './routes/signoutRoutes.js';
+//import signoutRoutes from './routes/signoutRoutes.js';
 // Route per verificare che l'utente abbia un token di accesso valido
-import verifyTokenRoutes from './routes/verifyTokenRoutes.js';
+//import verifyTokenRoutes from './routes/verifyTokenRoutes.js';
 // Route per verificare il numero di crediti disponibili
-import creditNumberRoutes from './routes/creditNumberRoutes.js';
+//import creditNumberRoutes from './routes/creditNumberRoutes.js';
 // Route per analizzare l'immagine tramite OPENAI
-import productImageRoutes from './routes/productImageRoutes.js';
+//import productImageRoutes from './routes/productImageRoutes.js';
 // Route per generazione il titolo e la descrizione
-import productRoutes from './routes/productRoutes.js';
+//import productRoutes from './routes/productRoutes.js';
 
 export const applyRoutes = (app) => {
     app.use('/', waitlistRoutes);
